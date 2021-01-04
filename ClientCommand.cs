04a0e993d9337862f05ca39d5acc0f4180a69f58
Blissful Mayer <blissfulmayer@pm.me>
2021-01-04 00:10:11 +0000

    Add the GetKeyPress command
    closes #1

--- ClientCommand.cs
+++ ClientCommand.cs
@@ -209,6 +209,7 @@ namespace LoU
         //ArmDisarmRight,
         WaitForTarget, // This is implemented client side! See ScriptDebugger.cs in EasyLoU project
         GetHotKey, // This is implemented client side! See ScriptDebugger.cs in EasyLoU project
+        GetKeyPress, // This is implemented client side! See ScriptDebugger.cs in EasyLoU project
         //TargetNext,
         //AttackLast,
         //Delay,
